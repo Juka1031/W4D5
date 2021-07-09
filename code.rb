7380307fbd8fd3b68556a69a5df63cc0fa3b02bd
julian <juka1031@gmail.com>
2021-07-09 14:23:08 -0400

    easy

--- code.rb
+++ code.rb
@@ -22,21 +22,36 @@ def my_min(list)
 end
 
 
-def largest_conti_subsum(list)
-    array = []
-    list.each_with_index do |ele, i| 
-        list.each_with_index do |ele2, j|
-            if j >= i   
-                array << list[i..j]
-            end
-        end
-    end
-    array.map! {|arr| arr.sum}.max
-end
+# def largest_conti_subsum(list)
+#     array = []
+#     list.each_with_index do |ele, i| 
+#         list.each_with_index do |ele2, j|
+#             if j >= i   
+#                 array << list[i..j]
+#             end
+#         end
+#     end
+#     array.map! {|arr| arr.sum}.max
+# end
 
 # testing
 
+def better_conti(list)
+    sum = 0
+    longest = 0
+    list.each do |ele|
+        if sum + ele > sum 
+            sum = sum + ele
+        else
+            longest = sum if sum > longest
+            sum = 0
+        end
+    end
+    longest
+end
 
+list = [2, 3, -1, 999 , -1000, 7, -6, 7]
+p better_conti(list)
 
 
 
